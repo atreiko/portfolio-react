7ecda6e7c2a4d7f1db0ff09446d9f4eb9644bb1a
atreiko <a.treiko92@gmail.com>
2022-08-28 11:43:16 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <HashRouter hashType='hashbang'>
     <App />
   </HashRouter>
 );
