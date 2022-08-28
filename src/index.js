2d7fd4f610bcda89e063af6f520fcb4b91fe4e92
atreiko <a.treiko92@gmail.com>
2022-08-28 13:46:52 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 import 'normalize.css';
 import './index.css';
 // hashType='hashbang'
+ // "homepage": "https://atreiko.github.io/portfolio-react",
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
